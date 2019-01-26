673481e4b92b66c53ece6de059ba03a8b05cacdc
Ggoings <42050294+Ggoings@users.noreply.github.com>
2019-01-26 17:37:07 -0600

    Add files via upload

---
+++ RPS-Game.js
@@ -0,0 +1,8 @@
+The game is Rock, Paper, Sissors.
+
+The user selects which hand they will make, computer generates a random pick, then the two choices will determine the outcome. 
+Then will displaying the winner and loser or if there was a tie.
+
+How this works is if the user choice is greater then the computers choice then it will display that the winner is the user. 
+If the users choice is lower then the computers choice then it will display that the user has lost. 
+But if the users choice equals the computers choice then the game is a tie and will ask to try again.
